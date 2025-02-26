48ea2e65ffbf7a8a2109045bbb1a7e49024db88b
Elena_Chr <e.chrysikou@atc.gr>
2025-02-26 11:51:21 +0200

    full widget 2

--- stencil.config.ts
+++ stencil.config.ts
@@ -8,6 +8,9 @@ dotenv.config();
 export const config: Config = {
   namespace: 'taktakwidget',
   globalStyle: 'src/global/global.css',
+  devServer: {
+    port: 80,
+  },
   env: {
     API_URL: process.env.API_URL,
     PLATFORM_URL: process.env.PLATFORM_URL,
